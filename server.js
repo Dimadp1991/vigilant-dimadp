0fcbac8c5b49da8655eda687aeee256fac87e299
Dimadp1991 <57724297+Dimadp1991@users.noreply.github.com>
2021-05-09 15:12:03 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require("path");
+const { send } = require('process');
+const { stringify } = require('querystring');
+const internal = require('stream');
+const app = express();
+
+const router = express.Router();
+
+const port = '3000';
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+router.get("/", (req, res) => res.sendFile(path.resolve("./index.html")));
+
+
+module.exports = router;
+var publicDir = require('path').join(__dirname, '/public');
+app.use(express.static(publicDir));
+app.use(router);
+app.use(express.json());
+app.listen(port, () => console.log(`server running on http://localhost:${port}`));
+
+
